0a0a1fae0a8453d787b4ebb193688b6b9df2dd76
xhy0410 <2939055420@qq.com>
2024-09-24 23:02:16 +0800

    Create 2.3.cpp

---
+++ 2.3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void butler(void); /*ANSI/ISO Cº¯ÊýÔ­ÐÍ*/
+int main(void)
+{
+	printf("i will summon the butler function.\n");
+	butler();
+	printf("yes. bring me some tea and writeable DVDs.\n");
+	
+	return 0;
+} 
+void butler(void)
+{
+	printf("you rang,sir?\n");
+}
