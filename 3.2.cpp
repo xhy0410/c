c730c868cfde12ddc64ae38b8ca921445d3de50c
xhy0410 <2939055420@qq.com>
2024-09-25 02:04:40 +0800

    Create 3.2.cpp

---
+++ 3.2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+	int ten=10;
+	int two=2;
+	
+	printf("doing it right:");
+	printf("%d minus %d is %d\n",ten,2,ten-two);
+	printf("doing it wrong:");
+	printf("%d minus %d is %d\n",ten);//ÒÅÂ©Á©²ÎÊı
+	
+	return 0; 
+}
